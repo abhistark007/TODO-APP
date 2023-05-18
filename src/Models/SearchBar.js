a226c1669fdda6365d795bf872455b9b905ce0f5
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 21:55:27 +0530

    made some changes to <Link> to attribute to extract the data at a later time

--- src/Models/SearchBar.js
+++ src/Models/SearchBar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import { Link } from 'react-router-dom';
 
-function SearchBar({text,textController,setTextController}) {
-  return (
-    <div className='w-full h-14 border-2 border-purple-600 flex items-center bg-white pl-3 rounded-full'>
-        <input placeholder={text} className='outline-none flex-[0.95]' onChange={(e)=>setTextController(e.target.value)} value={textController}/>
-        <button className='flex-[0.05] bg-black text-white h-full rounded-r-full'>
-            <SearchSharpIcon/>
-        </button>
-    </div>
-  )
+function SearchBar({ text, textController, setTextController, todoList }) {
+    return (
+        <div className='w-full h-14 border-2 border-purple-600 flex items-center bg-white pl-3 rounded-full'>
+            <input placeholder={text} className='outline-none flex-[0.95]' onChange={(e) => setTextController(e.target.value)} value={textController} />
+            
+                <button className='flex-[0.05] bg-black text-white h-full rounded-r-full'>
+                    <Link to={`/search/${textController}`}><SearchSharpIcon /></Link>
+                </button>
+            
+        </div>
+    )
 }
 
 export default SearchBar
