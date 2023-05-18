aa49acd60f9e6058eb10a3ddea747ad8f02d98de
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 22:20:56 +0530

    Added filter features based on filterOn value

--- src/Models/DisplayCard.js
+++ src/Models/DisplayCard.js
@@ -1,14 +1,42 @@
 import React, { useState } from 'react'
 import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 
-function DisplayCard({title,body,index,todoList,setTodoList}) {
+function DisplayCard({title,body,index,todoList,setTodoList,filterOn}) {
     const deleteTask=()=>{
         let tempList=Array.from(todoList);
         tempList.splice(index,1);
         setTodoList(tempList);
     }
     const [completed,setCompleted]=useState(false);
+
+
+    if(filterOn===true && completed===true){
+        return (
+    
+            <div className='flex flex-col gap-3 w-96 p-3 rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
+                <p className={`font-bold ${completed?"line-through":""}`}>{title}</p>
+                <p className={` ${completed?"line-through":""}`}>{body}</p>
+                <div className='flex justify-between items-center'>
+                    <label className='flex gap-2'>
+                        <input type='checkbox' onClick={(e)=>{
+                            setCompleted(!completed);
+                            // console.log(completed);
+                        }} value={completed}/>
+                        Completed?
+                    </label>
+        
+                    <button className='px-3 py-2 bg-pink-300 rounded-xl' onClick={deleteTask}>
+                        <DeleteSharpIcon/>
+                    </button>
+                </div>
+            </div>
+          )
+    }
+    if(filterOn===true && completed===false){
+        return <div className='hidden absolute'></div>
+    }
   return (
+    
     <div className='flex flex-col gap-3 w-96 p-3 rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
         <p className={`font-bold ${completed?"line-through":""}`}>{title}</p>
         <p className={` ${completed?"line-through":""}`}>{body}</p>
