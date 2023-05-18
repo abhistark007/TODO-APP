cbec0274ce2a415ee11d840b9042d491ef4f9d76
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 17:28:54 +0530

    Added the ability to delete each todo

--- src/Models/DisplayCard.js
+++ src/Models/DisplayCard.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 
-function DisplayCard({title,body}) {
+function DisplayCard({title,body,index,todoList,setTodoList}) {
+    const deleteTask=()=>{
+        let tempList=Array.from(todoList);
+        tempList.splice(index,1);
+        setTodoList(tempList);
+    }
   return (
     <div className='flex flex-col gap-3 w-96 p-3 rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
         <p>{title}</p>
@@ -12,7 +17,7 @@ function DisplayCard({title,body}) {
                 Completed?
             </label>
 
-            <button className='px-3 py-2 bg-pink-300 rounded-xl'>
+            <button className='px-3 py-2 bg-pink-300 rounded-xl' onClick={deleteTask}>
                 <DeleteSharpIcon/>
             </button>
         </div>
