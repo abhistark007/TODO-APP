34d54fc077d909b80c123f5fd11c27c4920a32c5
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 18:00:37 +0530

    removed for property as i am using tailwind

--- src/Models/TodoForm.js
+++ src/Models/TodoForm.js
@@ -5,7 +5,7 @@ function TodoForm({titleController,setTitleController,bodyController,setBodyCont
         <div className='flex flex-col gap-5'>
             <div className="flex items-center gap-3">
                 <div className="w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
                         Title
                     </label>
                 </div>
@@ -19,7 +19,7 @@ function TodoForm({titleController,setTitleController,bodyController,setBodyCont
 
             <div className="flex items-center  gap-3">
                 <div className="w-1/3">
-                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                    <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
                         Tasks
                     </label>
                 </div>
