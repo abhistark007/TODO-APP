2e9ad69c11d8a2e740fed935668f0681aa38bb3b
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 17:11:21 +0530

    Created Display Card to display each todos

---
+++ src/Models/DisplayCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
+
+function DisplayCard({title,body}) {
+  return (
+    <div className='flex flex-col gap-3 w-96 p-3 rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
+        <p>{title}</p>
+        <p>{body}</p>
+        <div className='flex justify-between items-center'>
+            <label className='flex gap-2'>
+                <input type='checkbox'/>
+                Completed?
+            </label>
+
+            <button className='px-3 py-2 bg-pink-300 rounded-xl'>
+                <DeleteSharpIcon/>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default DisplayCard
