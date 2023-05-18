4f416a40a0c41705e46a2a6d93a796e215bc626a
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-18 22:28:18 +0530

    Increased the clickable area of <Link> tag

--- src/Models/SearchBar.js
+++ src/Models/SearchBar.js
@@ -7,9 +7,11 @@ function SearchBar({ text, textController, setTextController, todoList }) {
         <div className='w-full h-14 border-2 border-purple-600 flex items-center bg-white pl-3 rounded-full'>
             <input placeholder={text} className='outline-none flex-[0.95]' onChange={(e) => setTextController(e.target.value)} value={textController} />
             
-                <button className='flex-[0.05] bg-black text-white h-full rounded-r-full'>
-                    <Link to={`/search/${textController}`}><SearchSharpIcon /></Link>
+                <Link to={`/search/${textController}`} className='flex flex-[0.05] bg-black text-white h-full rounded-r-full justify-center items-center'>
+                <button >
+                    <SearchSharpIcon />
                 </button>
+                </Link>
             
         </div>
     )
